3c516cf287d56c1e94b0e496b678acef457fc1dc
nikolai-nik <nikolaivista@gmail.com>
2017-12-20 14:40:38 +0200

    returns

--- assets/js/site-scripts.js
+++ assets/js/site-scripts.js
@@ -279,7 +279,21 @@
 	        perPage       : newPerPage
 	      });
 	    });
+	    
+		$(document).ready(function () {
+			 $('input,textarea').focus(function(){
+			   $(this).data('placeholder',$(this).attr('placeholder'))
+			   $(this).attr('placeholder','');
+			 });
+			 $('input,textarea').blur(function(){
+			   $(this).attr('placeholder',$(this).data('placeholder'));
+		 	});
+	 	});
 		
+
+		$('#datepicker').datepicker({
+            uiLibrary: 'bootstrap4'
+        });
 		
 	});// end ready
 
